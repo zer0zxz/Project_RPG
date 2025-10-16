ea70f6aa3fd422626045fea726adbc4fa02cdd77
zer0zxz <vitorlucasmazini@hotmail.com>
2025-10-16 16:46:39 -0400

    Primeira tentativa
    Leterally nothing there

---
+++ script.js
@@ -0,0 +1,13 @@
+let res = document.querySelector('div#resultado')
+
+function rolarDado(lados) {
+  const resultado = Math.floor(Math.random() * lados) + 1;
+  res.innerHTML = `🎲 Você rolou um D${lados}: ${resultado}`
+}
+
+document.querySelectorAll('.dado').forEach(dado => {
+  dado.addEventListener('click', () => {
+    const lados = parseInt(dado.id.replace('d', ''));
+    rolarDado(lados);
+  });
+});

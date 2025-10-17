366f605382b25c6eff081a6271975eb0ed0cbdba
zer0zxz <vitorlucasmazini@hotmail.com>
2025-10-17 15:14:00 -0400

    Coloquei som
    som de dado adicionado

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let res = document.querySelector('div#resultado')
 function rolarDado(lados) {
   const resultado = Math.floor(Math.random() * lados) + 1;
   //res.innerHTML = `🎲 Você rolou um D${lados}: ${resultado}`
+  const audio = new Audio('sounds/roll.mp3');
+  audio.playbackRate = 1.3;
+  audio.currentTime = 0;
+  audio.play();
   res.innerHTML = `${resultado}`
 }
 
